f68fd183ff163ef31fc4f58721326e838c450ca9
Leonardo Ozelin <leonardoozelin@gmail.com>
2018-03-16 13:03:00 -0300

    Update 16-03

--- frontEnd/src/app/nova-maquiagem/nova-maquiagem.component.ts
+++ frontEnd/src/app/nova-maquiagem/nova-maquiagem.component.ts
@@ -1,39 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { Mask } from './../teste/teste.component';
 
 
 @Component({
-  selector: 'app-nova-maquiagem',
-  templateUrl: './nova-maquiagem.component.html',
-  styleUrls: ['./nova-maquiagem.component.css']
+	selector: 'app-nova-maquiagem',
+	templateUrl: './nova-maquiagem.component.html',
+	styleUrls: ['./nova-maquiagem.component.css']
 })
 export class NovaMaquiagemComponent implements OnInit {
+	ValorTotal: any;
 
 	cliente = [
-		{value: '1', viewValue:'Leonardo Ozelin de Miranda'},
-		{value: '2', viewValue:'Antônio Correia Araujo'},
-		{value: '3', viewValue:'Gabrielly Cunha Gomes'},
-		{value: '4', viewValue:'Maria Goncalves Cunha'},
-		{value: '5', viewValue:' Yasmin Cunha Silva'},
+		{ value: '1', viewValue: 'Leonardo Ozelin de Miranda' },
+		{ value: '2', viewValue: 'Antônio Correia Araujo' },
+		{ value: '3', viewValue: 'Gabrielly Cunha Gomes' },
+		{ value: '4', viewValue: 'Maria Goncalves Cunha' },
+		{ value: '5', viewValue: ' Yasmin Cunha Silva' },
 	];
 
 	servico = [
-		{value: '1', viewValue:'Olho Preto Esfumado', valor:10.00},
-		{value: '2', viewValue:'Maquiagem Colorida', valor:11.50},
-		{value: '3', viewValue:'Delineado Gatinho', valor:12.00},
-		{value: '4', viewValue:'Olho Tudo Boca Nada', valor:13.00},
-		{value: '5', viewValue:'Maquiagem Neutra', valor:14.00},
-		{value: '6', viewValue:'Olho Tudo Boca Tudo', valor:15.00},
-		{value: '7', viewValue:'Maquiagem Discreta', valor:16.00},
-		{value: '8', viewValue:'Maquiagem Bronzeadora', valor:17.00},
-	]
+		{ value: '1', viewValue: 'Olho Preto Esfumado', valor: 10.00 },
+		{ value: '2', viewValue: 'Maquiagem Colorida', valor: 11.50 },
+		{ value: '3', viewValue: 'Delineado Gatinho', valor: 12.00 },
+		{ value: '4', viewValue: 'Olho Tudo Boca Nada', valor: 13.00 },
+		{ value: '5', viewValue: 'Maquiagem Neutra', valor: 14.00 },
+		{ value: '6', viewValue: 'Olho Tudo Boca Tudo', valor: 15.00 },
+		{ value: '7', viewValue: 'Maquiagem Discreta', valor: 16.00 },
+		{ value: '8', viewValue: 'Maquiagem Bronzeadora', valor: 17.00 },
+	];
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
+	aperta(event) {
+		var mask = new Mask();
 
-  ValorTotal: any;
+		mask.hora('hr', event);
+	}
 
 }
- 
+
--- frontEnd/src/app/teste/teste.component.ts
+++ frontEnd/src/app/teste/teste.component.ts
@@ -13,3 +13,50 @@ export class TesteComponent implements OnInit {
   }
 
 }
+export class Mask {
+	hora(campo, event) {
+		campo = document.getElementById(campo);
+		var novoValor = this.formataCampo(campo, "00:00", event);
+		if (novoValor.toString().length >= 5) {
+			var filtro = novoValor.toString().split(':');
+			if ((parseInt(filtro[0]) < 24) && (parseInt(filtro[1]) < 60)) {
+				campo.value = (filtro[0] + ":" + filtro[1]);
+			} else {
+				campo.value = "";
+				alert("Expressao invalida");
+			}
+		}
+	}
+
+	formataCampo(campo, Mascara, evento) {
+		var boleanoMascara;
+
+		var Digitato = evento.keyCode;
+		var exp = /\-|\.|\/|\(|\)| /g
+		var campoSoNumeros = campo.value.toString().replace(/\D/g, "");
+		var posicaoCampo = 0;
+		var NovoValorCampo = "";
+		var TamanhoMascara = campoSoNumeros.length;;
+
+		if (Digitato != 8) { // backspace 
+			for (var i = 0; i <= TamanhoMascara; i++) {
+				boleanoMascara = ((Mascara.charAt(i) == ":") || (Mascara.charAt(i) == ".")
+					|| (Mascara.charAt(i) == "/"))
+				boleanoMascara = boleanoMascara || ((Mascara.charAt(i) == "(")
+					|| (Mascara.charAt(i) == ")") || (Mascara.charAt(i) == " "))
+				if (boleanoMascara) {
+					NovoValorCampo += Mascara.charAt(i);
+					TamanhoMascara++;
+				} else {
+					NovoValorCampo += campoSoNumeros.charAt(posicaoCampo);
+					posicaoCampo++;
+				}
+			}
+			return NovoValorCampo;
+			//campo.value = NovoValorCampo;
+			//return true;
+		} else {
+			return true;
+		}
+	}
+}
